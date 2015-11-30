6770efe024db20fa05186ff3878e5094941598f2
Shuyang Shi <shuyang790@gmail.com>
2015-11-30 11:59:53 +0800

    manually merge two branches together using MACROs

--- README.md
+++ README.md
@@ -20,7 +20,30 @@ Social network de-anonymization (provided with an anonymized graph and a crawled
 		2. 否则，匹配该边并继续
 - 效果（略）
 
-## Current Status
+## Current Status: Use 'refinement' to match most vertices
+
+目前分为DNMC和BTCH两个版本。(通过`match.hpp`中的宏定义来控制)
+
+### DNMC: 每次匹配之后动态维护weights矩阵
+- 想法
+	- 利用原来的迭代算法，计算出seeds（一定比例）。(由该算法的特点可以知道seeds的度数一定较大且正确性较高。)
+	- 从seeds出发迭代匹配，每次匹配weights值最大的一对，匹配以后动态更新weights
+		- 更新的时候，使用的公式是`weights[x][y] += sim_nodes[u][v] * DECAY(iterno)`
+		- 其中，函数`DECAY`是一个不上升函数，自变量是迭代轮数
+	- 剩余未匹配的少量点用baseline匹配。
+- 效果
+	- 1 graph with 7500 vertices graph (50% overlap, precision upper bound 66.7%)
+		- Baseline: 45.83%
+		- `DECAY(i)=1`
+			- `perc_thrsd=0.07`, `DECAY(i)=1`: 52.45%. 提升6.62个百分点
+			- `perc_thrsd=0.05`, `DECAY(i)=1`: 51.56%. 提升5.73个百分点
+			- `perc_thrsd=0.10`, `DECAY(i)=1`: 51.52%. 提升5.69个百分点
+		- `DECAY(i) = (1 - 1e-4) ^ i`
+			- `perc_thrsd=0.07`, `DECAY(i) = (1 - 1e-4) ^ i`: 52.19%, 提升6.36个百分点
+			- `perc_thrsd=0.05`, `DECAY(i) = (1 - 1e-4) ^ i`: 52.41%, 提升6.58个百分点
+
+
+### BTCH: 每考虑若干对匹配之后重建weights矩阵
 
 - 思路
 	- 用baseline算法得到占比重`PERC_THRSD`的匹配对
@@ -31,7 +54,6 @@ Social network de-anonymization (provided with an anonymized graph and a crawled
 		- Baseline: 45.83%
 		- `PERC_THRSD=0.05`, `iter_cond=10`: 55.03%
 			- 提升了9.20个百分点
-
 ## Next Step
 
 - 理论上界计算
--- match.cpp
+++ match.cpp
@@ -110,8 +110,12 @@ void matcher::calc_sim_nodes_singleth(int i, int j, bool flag){
 		calc_sim_nodes(i, j, 1);
 }
 
-void matcher::record_matrix() {
-	FILE *ou = fopen("matrix.txt", "w");
+void matcher::record_matrix(char *filename) {
+	FILE *ou;
+	if (filename)
+	 	ou = fopen(filename, "w");
+	else
+		ou = fopen("matrix.txt", "w");
 	fprintf(ou, "%d %d\n", G_a->num_nodes, G->num_nodes);
 	for (int i=1; i<=G_a->num_nodes; i++){
 		for (int j=1; j<=G->num_nodes; j++)
@@ -119,17 +123,23 @@ void matcher::record_matrix() {
 		fprintf(ou, "\n");
 	}
 	fclose(ou);
-	fprintf(stderr, "matrix saved in `matrix.txt`.\n");
+	fprintf(stderr, "matrix saved in `%s`.\n",
+			filename ? filename : "matrix.txt");
 }
 
-void matcher::load_matrix() {
-	FILE *in = fopen("matrix.txt", "r");
+void matcher::load_matrix(char *filename) {
+	FILE *in;
+	if (filename)
+		in = fopen(filename, "r");
+	else
+	 	in = fopen("matrix.txt", "r");
 	fscanf(in, "%d%d", &(G_a->num_nodes), &(G->num_nodes));
 	for (int i = 1; i <= G_a->num_nodes; i++)
 		for (int j = 1; j <= G->num_nodes; j++)
 			fscanf(in, "%lf", sim_nodes[i]+j);
 	fclose(in);
-	fprintf(stderr, "Matrix read from `matrix.txt`\n");
+	fprintf(stderr, "Matrix read from `%s`\n",
+			filename ? filename : "matrix.txt");
 }
 
 void matcher::gen_sim_matrix_simranc() {
@@ -205,6 +215,9 @@ void matcher::gen_ans_pairs_oldway() {
 	delete []flag_a;
 	delete []flag;
 }
+
+// Process a batch of pairs after a re-establishment of `weights`
+#ifdef BTCH
 
 void matcher::gen_ans_pairs() {
 	clock_t time_start = clock();
@@ -327,6 +340,162 @@ iter:
 			(clock()-time_start)*1.0/CLOCKS_PER_SEC);
 }
 
+#endif
+
+// Dynamically maintain `weights` after each match
+#ifdef DNMC
+void matcher::gen_ans_pairs() {
+	clock_t time_start = clock();
+	ans_pairs.clear();
+	vector<match_edge> match_edges;
+	char * flag_a = new char[MAX_NODES], * flag = new char[MAX_NODES];
+	char * fake_flag_a = new char[MAX_NODES], * fake_flag = new char[MAX_NODES];
+	memset(flag_a, 0, MAX_NODES);
+	memset(flag, 0, MAX_NODES);
+	memset(fake_flag_a, 0, MAX_NODES);
+	memset(fake_flag, 0, MAX_NODES);
+
+	int * match = new int[MAX_NODES];
+
+	for (int i=1; i <= G_a->num_nodes; i++)
+		for (int j=1; j <= G->num_nodes; j++)
+			match_edges.push_back(match_edge(i, j, sim_nodes[i][j]));
+	sort(match_edges.begin(), match_edges.end());
+
+	for (vector <match_edge> :: iterator it=match_edges.begin();
+			it!=match_edges.end(); it++)
+		if (!fake_flag_a[it->u] && !fake_flag[it->v]) {
+			fake_flag_a[it->u] = 1;
+			fake_flag[it->v] = 1;
+			if (ans_pairs.size() < G_a->num_nodes * PERC_THRSD
+					&& !flag_a[it->u] && !flag[it->v]) {
+				flag_a[it->u] = 1;
+				flag[it->v] = 1;
+				match[it->u] = it->v;
+				ans_pairs.push_back(*it);
+			}
+			if (ans_pairs.size() > G_a->num_nodes * PERC_THRSD)
+				break;
+		}
+
+	fprintf(stderr, "First part: %lu pairs.\n", ans_pairs.size());
+
+	double TINY = 1e20;
+	for (size_t i = 1; i <= G_a->num_nodes; i++) {
+		for (int j = 1; j <= G->num_nodes; j++)
+			if (TINY > sim_nodes[i][j])
+				TINY = sim_nodes[i][j];
+	}
+
+	for (int i=1; i<=G_a->num_nodes; i++) {
+		if (!flag_a[i]) {
+			map <int, double> weight;
+			for (vector <int> :: iterator j = G_a->edges[i]->begin();
+					j != G_a->edges[i]->end(); j++)
+				if (flag_a[*j]) {
+					for (vector <int> :: iterator k = G->rev_edges[match[*j]]->begin();
+							k != G->rev_edges[match[*j]]->end(); k++)
+						if (!flag[*k])
+							weight[*k] += max(sim_nodes[i][*k], TINY);
+				}
+			for (vector <int> :: iterator j = G_a->rev_edges[i]->begin();
+					j != G_a->rev_edges[i]->end(); j++)
+				if (flag_a[*j]) {
+					for (vector <int> :: iterator k = G->edges[match[*j]]->begin();
+							k != G->edges[match[*j]]->end(); k++)
+						if (!flag[*k])
+							weight[*k] += max(sim_nodes[i][*k], TINY);
+				}
+			for (map <int, double> :: iterator k = weight.begin();
+					k!=weight.end(); k++)
+				weights[i][k->first] = k->second;
+		}
+	}
+
+	fprintf(stderr, "init weights.\n");
+
+	int iterno = 0;
+	H = new matcher::heap(weights, G_a->num_nodes, this);
+
+	for (int last_s = 0, u, v; H->len && ans_pairs.size() != last_s; iterno++) {
+		last_s = ans_pairs.size();
+		while (H->len){
+			u = H->nodes[1].u, v = H->nodes[1].v;
+			H->pop();
+			if (!flag_a[u] && !flag[v])
+				break;
+		}
+		if (flag_a[u] || flag[v])
+			break;
+		flag_a[u] = flag[v] = 1;
+		match[u] = v;
+		ans_pairs.push_back(match_edge(u, v, weights[u][v]));
+
+		for (vector <int> :: iterator j = G_a->edges[u]->begin();
+				j != G_a->edges[u]->end(); j++)
+			if (!flag_a[*j]) {
+				for (vector <int> :: iterator k = G->edges[v]->begin();
+						k != G->edges[v]->end(); k++)
+					if (!flag[*k]){
+						weights[*j][*k] += max(sim_nodes[*j][*k], TINY)
+								* DECAY(iterno);
+						if (H->heap_pos[*j][*k])
+							H->heap_up(H->heap_pos[*j][*k]);
+						else
+							H->push(*j, *k);
+					}
+			}
+
+		for (vector <int> :: iterator j = G_a->rev_edges[u]->begin();
+				j != G_a->rev_edges[u]->end(); j++)
+			if (!flag_a[*j]) {
+				for (vector <int> :: iterator k = G->rev_edges[v]->begin();
+						k != G->rev_edges[v]->end(); k++)
+					if (!flag[*k]){
+						weights[*j][*k] += max(sim_nodes[*j][*k], TINY)
+								* DECAY(iterno);
+						if (H->heap_pos[*j][*k])
+							H->heap_up(H->heap_pos[*j][*k]);
+						else
+							H->push(*j, *k);
+					}
+			}
+	}
+
+	fprintf(stderr, "%lu pairs matched.\n", ans_pairs.size());
+
+	for (std::vector<match_edge> :: iterator it=match_edges.begin();
+			it!=match_edges.end(); it++)
+			if (!flag_a[it->u] && !flag[it->v]){
+				flag_a[it->u] = flag[it->v] = 1;
+				match[it->u] = it->v;
+				ans_pairs.push_back(*it);
+			}
+	fprintf(stderr, "%lu pairs processed.\n", ans_pairs.size());
+
+	for (int i=1; i<=G_a->num_nodes; i++)
+		if (!flag_a[i]) {
+			for (int j=1; j<=G->num_nodes; j++)
+				if (!flag[j]) {
+					flag_a[i] = flag[j] = 1;
+					match[i] = j;
+					ans_pairs.push_back(match_edge(i, j, 0));
+					break;
+				}
+		}
+
+	delete H;
+
+	delete []match;
+	delete []flag_a;
+	delete []flag;
+	delete []fake_flag_a;
+	delete []fake_flag;
+	fprintf(stderr, "answer pairs generated.\n\t%.2lf seconds.\n",
+			(clock()-time_start)*1.0/CLOCKS_PER_SEC);
+}
+#endif
+
 void matcher::print(FILE *ou) {
 	for (vector<match_edge> :: iterator it=ans_pairs.begin();
 			it!=ans_pairs.end(); ++it) {
@@ -338,3 +507,79 @@ void matcher::print(FILE *ou) {
 
 	}
 }
+
+// ===============
+// matcher::heap
+// heap operations
+
+#ifdef DNMC
+matcher::heap::heap(class matcher *o) {
+	len = 0;
+	owner = o;
+}
+
+matcher::heap::heap(int n, int m, class matcher *o) {
+	owner = o;
+	len = 0;
+	memset(heap_pos, 0, sizeof(heap_pos));
+	for (int i=1; i<=n; i++)
+		for (int j=1; j<=m; j++){
+			heap_pos[i][j] = ++len;
+			nodes[len] = heap_node(i, j);
+		}
+	for (int i=len/2; i>0; i--)
+		heap_down(i);
+}
+
+matcher::heap::heap(map <int, double> *weights, int n, class matcher *o) {
+	owner = o;
+	len = 0;
+	for (int i=1; i<=n; i++){
+		for (std::map<int, double> ::iterator it=weights[i].begin();
+			it != weights[i].end(); it++) {
+			heap_pos[i][it->first] = ++len;
+			nodes[len] = heap_node(i, it->first);
+		}
+	}
+	for (int i=len/2; i>0; i--)
+		heap_down(i);
+}
+
+void matcher::heap::push(int u, int v) {
+	int pos = heap_pos[u][v];
+	if (pos <= len && nodes[pos].u == u && nodes[pos].v == v)
+		return;
+	nodes[++len] = heap_node(u, v);
+	heap_pos[u][v] = len;
+	heap_up(len);
+}
+
+void matcher::heap::heap_down(int x) {
+	for (int ma; heap_lc(x) <= len; x = ma){
+		ma = heap_rc(x) <= len
+			? (heap_v(heap_lc(x)) > heap_v(heap_rc(x))
+				? heap_lc(x) : heap_rc(x))
+			: heap_lc(x);
+		if (heap_v(x) >= heap_v(ma))
+			break;
+		swap(nodes[x], nodes[ma]);
+		swap(heap_p(x), heap_p(ma));
+	}
+}
+
+void matcher::heap::heap_up(int x){
+	for (; heap_fa(x)>0 && heap_v(x) > heap_v(heap_fa(x)); ){
+		swap(nodes[x], nodes[heap_fa(x)]);
+		swap(heap_p(x), heap_p(heap_fa(x)));
+	}
+}
+
+void matcher::heap::pop(){
+	if (len <= 0)
+		return;
+	swap(nodes[1], nodes[len]);
+	swap(heap_p(1), heap_p(len));
+	len--;
+	heap_down(1);
+}
+#endif
